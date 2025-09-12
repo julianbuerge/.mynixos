47ff3b97c99db61c1a2024943b42a2e86820b722
julianbuerge <julian.buerge@alumni.ethz.ch>
2025-09-12 15:36:37 +0200

    updated filesystem mounting

--- hosts/panther/filesystems.nix
+++ hosts/panther/filesystems.nix
@@ -2,11 +2,12 @@
 { config, ... }:
 let
 
-homedir = builtins.getEnv "HOME";
+variables = import ./variables.nix;
+username = variables.username;
 
 in {
     #mount my data drive
-    fileSystems."${homedir}/Data" = {
+    fileSystems."/home/${username}/Data" = {
 
 	device = "/dev/disk/by-uuid/b2c7a814-a946-4e20-aa63-4f23d1b06a25";
 	fsType = "ext4";
