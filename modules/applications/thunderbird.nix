fdb3b30d0117407cd2172ec4b8a9db3fed4ffa3a
julianbuerge <julian.buerge@alumni.ethz.ch>
2025-12-13 09:28:05 +0100

    deleted obsolete comments

--- modules/applications/thunderbird.nix
+++ modules/applications/thunderbird.nix
@@ -6,7 +6,6 @@
   programs.thunderbird = {
     enable = true;
 
-    # Enterprise policies (JSON-like)
     policies = {
       # Disable telemetry, startup pages, etc.
       DisableTelemetry = true;
@@ -24,21 +23,16 @@
 
     # Preferences = user.js style (about:config keys)
     preferences = {
-      # Disable remote content in mail
       "mailnews.message_display.disable_remote_image" = true;
 
-      # Disable cookies
       "network.cookie.cookieBehavior" = 2; # 0=accept all, 1=only from origin, 2=block all
 
-      # Don’t show start page
       "mailnews.start_page.enabled" = false;
       "mailnews.start_page.override_url" = "";
       "mailnews.start_page.url" = "";
 
-      # Don’t check if Thunderbird is default mail client
       "mail.shell.checkDefaultClient" = false;
 
-      # Threading / view style
       "mailnews.default_news_sort_order" = 2; # 2=by date
       "mailnews.default_news_view_flags" = 1; # 1=threaded, 0=flat
       "mailnews.default_sort_order" = 2;
