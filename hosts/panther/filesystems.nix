2b7ea92536d05c5a33f46d4729672c7afb24376c
julianbuerge <julian.buerge@alumni.ethz.ch>
2025-12-23 10:11:47 +0100

    Added my media library drive to the filesystems.

--- hosts/panther/filesystems.nix
+++ hosts/panther/filesystems.nix
@@ -1,16 +1,21 @@
 #these options will modify fstab.
-{ config, variables, ... }:
-let
-    inherit (variables) username;
-in
 {
-    #mount my data drive
-    fileSystems."/home/${username}/Data" = {
-
-	device = "/dev/disk/by-uuid/b2c7a814-a946-4e20-aa63-4f23d1b06a25";
-	fsType = "ext4";
-	options = [ "nofail" "defaults" ];
-
-    };
-
+  config,
+  variables,
+  ...
+}: let
+  inherit (variables) username;
+in {
+  #mount my data drive
+  fileSystems."/home/${username}/Data" = {
+    device = "/dev/disk/by-uuid/b2c7a814-a946-4e20-aa63-4f23d1b06a25";
+    fsType = "ext4";
+    options = ["nofail" "defaults"];
+  };
+  #mount my media library drive
+  fileSystems."/home/${username}/MediaLibrary" = {
+    device = "/dev/disk/by-uuid/0b570c9e-035a-4518-ac1b-40cdcee6eb1e";
+    fsType = "ext4";
+    options = ["nofail" "noatime" "defaults"];
+  };
 }
--- hosts/xenia/filesystems.nix
+++ hosts/xenia/filesystems.nix
@@ -1,25 +1,27 @@
 #these options will modify fstab.
-{ config, variables, ... }:
-let
-    inherit (variables) username;
-in
 {
-    #mount my data drive
-    fileSystems."/home/${username}/Data" = {
-
-	device = "/dev/disk/by-uuid/b2c7a814-a946-4e20-aa63-4f23d1b06a25";
-	fsType = "ext4";
-	options = [ "nofail" "defaults" ];
-
-    };
-    #mount my backup & games drive
-    fileSystems."/home/${username}/SecondaryDrive" = {
-
-	device = "/dev/disk/by-uuid/a05c41da-9ff8-45c1-91d4-a251ef0653d0";
-	fsType = "ext4";
-	options = [ "nofail" "defaults" ];
-
-    };
-
-
+  config,
+  variables,
+  ...
+}: let
+  inherit (variables) username;
+in {
+  #mount my data drive
+  fileSystems."/home/${username}/Data" = {
+    device = "/dev/disk/by-uuid/b2c7a814-a946-4e20-aa63-4f23d1b06a25";
+    fsType = "ext4";
+    options = ["nofail" "defaults"];
+  };
+  #mount my media library drive
+  fileSystems."/home/${username}/MediaLibrary" = {
+    device = "/dev/disk/by-uuid/0b570c9e-035a-4518-ac1b-40cdcee6eb1e";
+    fsType = "ext4";
+    options = ["nofail" "noatime" "defaults"];
+  };
+  #mount my backup & games drive
+  fileSystems."/home/${username}/SecondaryDrive" = {
+    device = "/dev/disk/by-uuid/a05c41da-9ff8-45c1-91d4-a251ef0653d0";
+    fsType = "ext4";
+    options = ["nofail" "defaults"];
+  };
 }
