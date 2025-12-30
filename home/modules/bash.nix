ee55cc1acc32cf804e7001f848a468601bb3c2df
julianbuerge <julian.buerge@alumni.ethz.ch>
2025-12-30 23:03:36 +0100

    Improve trash removal alias in bash.nix.

--- home/modules/bash.nix
+++ home/modules/bash.nix
@@ -10,10 +10,12 @@
       tree3 = "tree -L 3";
       tree4 = "tree -L 4";
       matrix = "cmatrix -B -C magenta -u 3";
-      rmHomeTrash = "rm -rf ~/.local/share/Trash/files/*; rm -rf ~/.local/share/Trash/info/*";
-      rmDataTrash = "rm -rf ~/Data/.Trash-1000/files/.*; rm -rf ~/Data/.Trash-1000/info/.*";
-      rmMediaTrash = "rm -rf ~/MediaLibrary/.Trash-1000/files/.*; rm -rf ~/MediaLibrary/.Trash-1000/info/.*";
-      rmBackupsTrash = "rm -rf ~/Backups/.Trash-1000/files/.*; rm -rf ~/Backups/.Trash-1000/info/.*";
+
+      #Every removal happens twice, once vanilla, once hidden files
+      rmHomeTrash = "rm -rf ~/.local/share/Trash/files/*; rm -rf ~/.local/share/Trash/info/*; rm -rf ~/.local/share/Trash/files/.*; rm -rf ~/.local/share/Trash/info/.*";
+      rmDataTrash = "rm -rf ~/Data/.Trash-1000/files/*; rm -rf ~/Data/.Trash-1000/info/*; rm -rf ~/Data/.Trash-1000/files/.*; rm -rf ~/Data/.Trash-1000/info/.*";
+      rmMediaTrash = "rm -rf ~/MediaLibrary/.Trash-1000/files/*; rm -rf ~/MediaLibrary/.Trash-1000/info/*; rm -rf ~/MediaLibrary/.Trash-1000/files/.*; rm -rf ~/MediaLibrary/.Trash-1000/info/.*";
+      rmBackupsTrash = "rm -rf ~/Backups/.Trash-1000/files/*; rm -rf ~/Backups/.Trash-1000/info/*; rm -rf ~/Backups/.Trash-1000/files/.*; rm -rf ~/Backups/.Trash-1000/info/.*";
       mynix = "cd ~/.mynixos/";
     };
   };
